a597d2e8287a05e55246045ff58022aee86aa5e5
Poci Knob <YOUR_GITHUB_EMAIL>
2025-09-23 21:26:40 +0100

    Bump to 0.14-nuc and add NUC status line

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 namespace wb {
     constexpr const char* kAppName = "Waterbutt Controller";
-    constexpr const char* kVersion = "0.12-hello";
+    constexpr const char* kVersion = "0.13-nuc";
 
     int run(int /*argc*/, char** /*argv*/) {
         std::cout << "----------------------------------------\n";
